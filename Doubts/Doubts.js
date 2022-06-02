79b98c7c175b2eaf0c93af33967e478a01482711
shubham2o <shubhamsharma.ha@gmail.com>
2022-06-02 18:16:04 +0530

    HTML canvas drawImage() Method added

--- Doubts/Doubts.js
+++ Doubts/Doubts.js
@@ -9,3 +9,13 @@ document.getElementById("demo").innerHTML =
 let language2 = navigator.language;
 document.getElementById("demo2").innerHTML =
     "Browser language : " + language2;
+
+
+// HTML canvas drawImage() Method
+// EXAMPLE 1 ||
+window.onload = function () {
+    var c = document.getElementById('myCanvas');
+    var ctx = c.getContext("2d");
+    var img = document.getElementById('scream');
+    ctx.drawImage(img, 10, 10);
+}
