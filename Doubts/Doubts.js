a59228ae6b7a0b4c2c06c13ddfe4a00fe7d7679a
shubham2o <shubhamsharma.ha@gmail.com>
2022-06-02 12:51:47 +0530

    navigator+Diff. window&navigator | window&document

---
+++ Doubts/Doubts.js
@@ -0,0 +1,11 @@
+// Window Navigator
+// EXAMPLE 1 ||
+let language = window.navigator.language;
+document.getElementById("demo").innerHTML =
+    "Browser language : " + language;
+
+
+// EXAMPLE 2 ||
+let language2 = navigator.language;
+document.getElementById("demo2").innerHTML =
+    "Browser language : " + language2;
