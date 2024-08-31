d471958f5691642e4091661bac34f9684fa9b87c
shubham2o <2contactshubham@gmail.com>
2024-08-31 23:49:05 +0530

    Code refactored + Minor changes done

--- script.js
+++ script.js
@@ -6,18 +6,11 @@ const snap = document.querySelector('.snap');
 
 function getVideo() {
     navigator.mediaDevices.getUserMedia({
-            video: true,
-            audio: false
-        })
+        video: true,
+        audio: false
+    })
         .then(localMediaStream => {
             console.log(localMediaStream);
-
-            //  DEPRECIATION : 
-            //       The following has been depreceated by major browsers as of Chrome and Firefox.
-            //       video.src = window.URL.createObjectURL(localMediaStream);
-            //       Please refer to these:
-            //       Deprecated  - https://developer.mozilla.org/en-US/docs/Web/API/URL/createObjectURL
-            //       Newer Syntax - https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/srcObject
 
             video.srcObject = localMediaStream;
             video.play();
@@ -35,16 +28,8 @@ function paintToCanvas() {
 
     return setInterval(() => {
         ctx.drawImage(video, 0, 0, width, height);
-        // take the pixels out
         let pixels = ctx.getImageData(0, 0, width, height);
-        // mess with them
-        // pixels = redEffect(pixels);
-
         pixels = rgbSplit(pixels);
-        // ctx.globalAlpha = 0.8;
-
-        // pixels = greenScreen(pixels);
-        // put them back
         ctx.putImageData(pixels, 0, 0);
     }, 16);
 }
